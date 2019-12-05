4c33a2232a5d3a7974875b7b37239a58b603b0cb
Fabio Dionisio <fabiogdionisio@gmail.com>
2019-12-05 20:19:41 -0300

    Rota de criação, edição e autorização do usuário

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'ab52d072f090fe86ca9c8898d129cc36',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,15 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Rocket Seat' }));
+routes.post('/users', UserController.store);
+routes.put('/users', authMiddleware, UserController.update);
+
+routes.post('/sessions', SessionController.store);
 
 export default routes;
